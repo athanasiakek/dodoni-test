aa66079f0af43216f2ab550215da2f0e03d27401
athanasiakek <121031319+athanasiakek@users.noreply.github.com>
2024-07-18 15:28:26 +0000

    init commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
+import random
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+title = st.text_input("Ψηφιακό Μαντείο Δωδώνης", "...")
+
+
+n = random.random() 
+
+if(title != '...'):
+    st.write("Η ερώτηση σου:....", n)
